25328727492f481645f77df1bdbbfa7938abd8d7
GuilhermeSilva99 <guiobmep@gmail.com>
2022-12-20 01:44:24 -0300

    Adicionando leitura de jogada

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     /**
      * Script para se basear durante o desenvolvimento:
      *
      * Repetir até o jogo acabar
      *      imprimir o jogo - ok -
-     *      saber qual jogador da vez
+     *      saber qual jogador da vez - ok -
+     *      ler jogada
      *      validar a jogada
      *          Se está vazio
      *          Se a posição existe
@@ -19,12 +22,26 @@ public class Main {
      *          
      */
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Character jogador1 = 'O';
         Character jogador2 = 'X';
 
         Character[][] jogo = new Character[3][3];
+        Character jogadorAtual = jogador1;
+        int linha = 0;
+        int coluna = 0;
 
-        imprimir(jogo);
+        boolean jogoContinua = true;
+
+        while (jogoContinua){
+            System.out.println("Digite a linha:");
+            linha = scanner.nextInt();
+            System.out.println("Digite a coluna:");
+            coluna = scanner.nextInt();
+        }
+
+
+
 
     }
     public static void imprimir(Character[][] matriz){

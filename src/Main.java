56e40538f1e57b4c404046f49e484e671b28b438
GuilhermeSilva99 <guiobmep@gmail.com>
2022-12-20 00:37:50 -0300

    Criando o script para basear o desenvolvimento

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,23 @@
 public class Main {
+    /**
+     * Script para se basear durante o desenvolvimento:
+     *
+     * Repetir até o jogo acabar
+     *      imprimir o jogo
+     *      saber qual jogador da vez
+     *      validar a jogada
+     *          Se está vazio
+     *          Se a posição existe
+     *      ver se o jogo acabou
+     *          Se alguem ganhou
+     *              verificar linhas
+     *              verificar colunas
+     *              verificar diagonais
+     *          Se deu velha
+     *      Se não acabou
+     *          trocar de jogador
+     *          
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }

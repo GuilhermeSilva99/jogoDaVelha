59bfbc39e90ef3768e7b68e0cfaa08f62003c4a9
GuilhermeSilva99 <guiobmep@gmail.com>
2022-12-20 01:33:06 -0300

    Criando o metodo pra imprimir a matriz

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
      * Script para se basear durante o desenvolvimento:
      *
      * Repetir até o jogo acabar
-     *      imprimir o jogo
+     *      imprimir o jogo - ok -
      *      saber qual jogador da vez
      *      validar a jogada
      *          Se está vazio
@@ -23,6 +23,25 @@ public class Main {
         Character jogador2 = 'X';
 
         Character[][] jogo = new Character[3][3];
-        
+
+        imprimir(jogo);
+
+    }
+    public static void imprimir(Character[][] matriz){
+        System.out.println("#|0|1|2");
+        for (int i = 0; i <matriz.length; i++) {
+            System.out.print(i+" ");
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] == null){
+                    System.out.print("_");
+                }else {
+                    System.out.print(matriz[i][j]);
+                }
+                if(j != matriz[i].length -1){
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+        }
     }
 }
